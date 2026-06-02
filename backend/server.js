fe58b4d1d2ba4df11f8cb009b6f130805b71b0ba
EnderZ-013 <indrazorc@gmail.com>
2026-06-02 23:18:11 +0200

    Autentifikimi

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
-
+const authRoutes = require("./routes/authRoutes");
 const connectDB = require("./config/db");
-
+const protect = require("./middleware/authMiddleware");
 dotenv.config();
 
 connectDB();
@@ -12,13 +12,18 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 const PORT = process.env.PORT || 5000;
-
+app.get("/api/profile", protect, (req, res) => {
+  res.json({
+    message: "Protected route accessed",
+    user: req.user,
+  });
+});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
